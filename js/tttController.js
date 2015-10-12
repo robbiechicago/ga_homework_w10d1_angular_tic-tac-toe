753228cbd398144a8c13a6f6cf1dd75bf8c304d8
Rob Holloway <robbiechicago@gmail.com>
2015-10-12 19:45:15 +0100

    clicking cells in the table console logs nought then cross then nought then cross etc.  nice

--- js/tttController.js
+++ js/tttController.js
@@ -1,6 +1,20 @@
 angular.module('tttApp', [])
   .controller('TttController', TttController);
 
-TttController = function() {
-  
-}
+function TttController() {
+  var ox = 'NOUGHT'
+
+  this.move = move
+
+  function move() {
+    if (ox === 'NOUGHT') {
+      ox = "CROSS";
+      console.log(ox);
+    } else {
+      ox = "NOUGHT";
+      console.log(ox);
+    };
+  };
+
+
+};
